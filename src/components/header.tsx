600f8a4122f6f938077d3a4d9416103a0e535121
davso11 <david.sehi2016@gmail.com>
2024-01-15 19:28:55 +0000

    style: change header image height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,19 @@
 export const Header = () => {
   return (
     <header>
-      <div className="bg-primary w-full">
+      <div className="w-full bg-primary">
         {/* Desktop */}
         <img
           alt="Banner"
           src="/images/bg-header-desktop.svg"
-          className="hidden h-40 w-full object-cover md:block"
+          className="hidden h-[158px] w-full object-cover md:block"
         />
 
         {/* Mobile */}
         <img
           alt="Banner"
           src="/images/bg-header-mobile.svg"
-          className="h-40 w-full object-cover md:hidden"
+          className="h-[158px] w-full object-cover md:hidden"
         />
       </div>
     </header>
